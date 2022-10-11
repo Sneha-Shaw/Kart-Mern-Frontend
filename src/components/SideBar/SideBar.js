71e9c889a9793ae5391f8cd8dd8a2069465431d6
Sneha Shaw <snehashaw122@gmail.com>
2022-10-11 22:41:37 +0530

    `feat`:`added logout feature`

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,9 +4,13 @@ import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { logoutUser } from '../../redux/actions/userAction';
+import { useDispatch } from 'react-redux';
 
 const SideBar = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
   return (
     // my details, my orders, account settings> logout
     <div className={classes.container}>
@@ -14,7 +18,7 @@ const SideBar = () => {
       <li className={classes.listItem}><a href='/profile'><ManageAccountsIcon fontSize='large' sx={{paddingRight: "2rem"}} />Account Settings</a></li>
         <li className={classes.listItem}><a href='/orders'><ListAltIcon fontSize='large' sx={{paddingRight: "2rem"}} />My Orders</a></li>
         <li className={classes.listItem}><a href='/notifications'><NotificationsActiveIcon fontSize='large' sx={{paddingRight: "2rem"}} />Notifications</a></li>
-        <li className={classes.listItem}><a href='/notifications'><LogoutIcon fontSize='large' sx={{paddingRight: "2rem"}} />Logout</a></li>
+        <li className={classes.listItem}><a href='/' onClick={()=>dispatch(logoutUser())}><LogoutIcon fontSize='large' sx={{paddingRight: "2rem"}} />Logout</a></li>
         </ul>
     </div>
   )
