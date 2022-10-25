47c4470a362f173133fbca3adb6dabe99eb26b9a
Sneha Shaw <snehashaw122@gmail.com>
2022-10-25 22:54:22 +0530

    `feat`: getting wishlist products

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,69 +1,44 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import useStyles from "./styles";
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Button, Card, CardMedia, CardContent } from '@mui/material';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import CardComponent from '../../components/Card/CardComponent'
+
+import { useNavigate } from 'react-router-dom';
+import {
+  getWishlist
+} from '../../redux/actions/productAction';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Wishlist = () => {
-  const classes = useStyles();
-  const products = [
-    // { "name": "T-shirt", "price": "200", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-    // { "name": "T-shirt", "price": "300", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-    // { "name": "T-shirt", "price": "400", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-    { "name": "T-shirt", "price": "400", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-    { "name": "T-shirt", "price": "400", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-    { "name": "T-shirt", "price": "400", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
-  ]
-  const [color, setColor] = React.useState(false)
+  const classes = useStyles()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { wishlistProduct } = useSelector(state => state.getWishlist)
+  const { userInfo } = useSelector(state => state.signInUser)
+  const { data: deleteProduct, error } = useSelector(state => state.deleteProductFromCart)
+  useEffect(() => {
+    if (!error) {
+
+      setTimeout(() => {
+        dispatch(getWishlist(userInfo?._id))
+      }, 1000)
+    }
+  }, [deleteProduct, dispatch, error])
+  useEffect(() => {
+    if (userInfo) {
+      dispatch(getWishlist(userInfo?._id))
+    } else {
+      navigate('/login')
+    }
+  }, [dispatch, userInfo, navigate])
+
   return (
     <div className={classes.container}>
       <div className={classes.products}>
-        {products &&
-          products.map(
+        {wishlistProduct &&
+          wishlistProduct.body.map(
             (product, index) => {
               return (
-
-                <Card key={index}
-                  sx={{ margin: "4rem" }}
-                // onClick={() => {
-                //     redirectHandler(
-                //         product.course.courseSlug
-                //     )
-                // }}
-                >
-                  {/* <div className={classes.imgContainer}>
-                                        <img src={product.img} alt="thumbnail" width="100%" height="100%" />
-                                    </div> */}
-                  {/* {
-                    color ?
-                      <IconButton aria-label="add to favorites"  onClick={() => setColor(!color)}>
-                        <FavoriteIcon className={classes.wish} sx={{color: "red"}} />
-                      </IconButton>
-                      :
-                      <IconButton aria-label="add to favorites"onClick={() => setColor(!color)}>
-                        <FavoriteBorderIcon   className={classes.wish}  sx={{color: "red"}}/>
-                      </IconButton>
-                  } */}
-
-
-                  <CardMedia
-                    component="img"
-                    // height="194"
-                    image={product.img}
-                  />
-
-                  <CardContent className={classes.details}>
-                    <h3 className={classes.title}>{product.name}</h3>
-                    <div className={classes.priceContainer}>
-                      <p className={classes.price}>₹{product.price}</p>
-                      <Button sx={{ fontSize: "1.6rem" }} variant="contained">Buy Now</Button>
-
-                    </div>
-                  </CardContent>
-
-
-                </Card>
+                <CardComponent product={product.productId} key={index} />
 
               );
             })}
--- src/pages/Wishlist/styles.js
+++ src/pages/Wishlist/styles.js
@@ -3,96 +3,11 @@ export default makeStyles((theme) => ({
     container: {
         width: "100%"
     },
-    introContainer: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        background: "black",
-        color: "white"
-    },
-    textPart: {
-        width: "50%",
-        fontSize: "7rem",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        // textAlign: "left"
-        "&>Button":{
-            borderRadius: "10rem",
-            fontSize: "2rem",
-            marginTop: "2rem"
-        }
-    },
-    imgPart: {
-        width: "40%",
-        padding: "2.1rem"
-    },
-    rectangle: {
-        background: "#E7F6F2",
-        width: "40rem",
-        height: "60rem",
-        borderRadius: "10%",
 
-
-    },
-    offer: {
-        background: "#A5C9CA",
-        borderRadius: "2.5rem 2.5rem .5rem 2.5rem",
-        padding: "2rem",
-        lineHeight: "1.5",
-        fontSize: "2rem",
-        width: "15%",
-        color: "black",
-        position: "absolute",
-        top: "10rem",
-        left: "66rem"
-    },
-    img: {
-        width: "91rem",
-        position: "absolute",
-        top: "7rem",
-        right: "0"
-    },
-    filterContainer: {
-        width: "50%",
-        display: "flex",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        padding: "2rem 0",
-        margin: "auto"
-    },
-    sort: {
-        width: "40%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        "&>select": {
-            width: "50%",
-            padding: "1rem",
-            borderRadius: ".5rem",
-            fontSize: "1.5rem",
-            border: "none"
-        }
-    },
-    filter: {
-        width: "30%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        "&>select": {
-            width: "50%",
-            padding: "1rem",
-            borderRadius: ".5rem",
-            fontSize: "1.5rem",
-            border: "none"
-
-        }
-    },
     products: {
         width: "100%",
         display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
+        gridTemplateColumns: "repeat(6, 1fr)",
         // gridGap: "2rem",
         padding: "2rem 0",
 
@@ -100,51 +15,6 @@ export default makeStyles((theme) => ({
     },
 
 
-    details: {
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        // alignItems: "center",
-        textAlign: "left",
-        padding: "2rem 0",
-        background: "#A5C9CA",
-        lineHeight: "2",
-        "&>Button": {
-            padding: "2rem"
-
-        }
-
-    },
-    priceContainer: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        "&>Button": {
-            marginRight: "5rem",
-            fontSize: "1.5rem",
-            background: "#395B64"
-        }
-
-    },
-    title: {
-        fontSize: "3rem",
-        padding: " 0 2rem",
-        fontWeight: "bold",
-        color: "#395B64"
-    },
-    price: {
-        fontSize: "4rem",
-        padding: " 0 2rem",
-        fontWeight: "bold",
-        color: "#395B64"
-    },
-    wish:{
-        // position: "absolute",
-        // top: "1rem",
-        // left: "37rem",
-        fontSize: "3rem !important",
-    }
 
 }
 )
