d0cad5790e5237a4e34dc145c544f543ab818086
Sneha Shaw <snehashaw122@gmail.com>
2022-10-26 19:45:26 +0530

    `fix`: `all otrder`

--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -6,7 +6,7 @@ import { Card, CardMedia, CardContent } from '@mui/material';
 
 const OrderPage = () => {
   const classes = useStyles()
-  const orderFilter = ["Shipped", "Delivered", "Cancelled", "Returned"]
+  const orderFilter = ["All orders","Pending","Shipped", "Delivered", "Cancelled", "Returned"]
   const orderproducts = [
     { "name": "T-shirt", "price": "200", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
     { "name": "T-shirt", "price": "300", "img": "https://images.squarespace-cdn.com/content/v1/59d2bea58a02c78793a95114/1533744372275-89SQDE9JMMSO09CMNBOS/%28RED%29_Men%27s%2BINSPI%28RED%29%2BTshirt.jpg" },
