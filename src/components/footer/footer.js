316b7940e22df0a9d7bd39eab73cc9df05b34b36
Sneha Shaw <snehashaw122@gmail.com>
2023-04-22 18:49:36 +0530

    `footer`

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,41 +1,111 @@
 import { Button } from '@mui/material';
 import React from 'react'
 import useStyles from "./styles";
-import jeans from '../../assets/jeans.jpg'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.container}>
-      <div className={classes.offer}>
-        {/* <h4>Get 50% off on your first order</h4> */}
-        <h4>Subscribe to our newsletter and get 50% off on your first order</h4>
-        <input type="text" placeholder="Enter your email" />
-        <Button variant="contained">Subscribe</Button>
+      <div className={classes.top}>
+        <div className={classes.Subscribe}>
+          <h3>Subscribe to our newsletter</h3>
+          <p>Get the latest news and offers</p>
+          <div className={classes.input}>
+            <input type="text" placeholder="Enter your email address" />
+            <Button variant="contained" color="primary">Subscribe</Button>
+          </div>
+        </div>
+        <div className={classes.social}>
+          <h3>Follow us</h3>
+          <div className={classes.icons}>
+            <Link to="#" target='_blank'><i className="fab fa-facebook-f"></i></Link>
+            <Link to="#" target='_blank'><i className="fab fa-twitter"></i></Link>
+            <Link to="#" target='_blank'><i className="fab fa-instagram"></i></Link>
+            <Link to="#" target='_blank'><i className="fab fa-youtube"></i></Link>
+          </div>
+        </div>
       </div>
-      <div className={classes.footer}>
-        <ul className={classes.footerLeft}>
-          <h4>Get to know us</h4>
-          <li><a href="/">About us</a></li>
-          <li><a href="/">Blog</a></li>
-          <li><a href="/">Press</a></li>
-          <li><a href="/">Careers</a></li>
-          <li><a href="/">Terms of Service</a></li>
-          <li><a href="/">Privacy Policy</a></li>
-        </ul>
-        <ul className={classes.footerRight}>
-          <h4>Connect with us</h4>
-          <li><a href="/">Facebook</a></li>
-          <li><a href="/">Twitter</a></li>
-          <li><a href="/">Instagram</a></li>
-          <li><a href="/">Youtube</a></li>
-          <li><a href="/">Linkedin</a></li>
-        </ul>
+      <hr />
+      <div className={classes.links}>
+        <div className={classes.link}>
+          <h3>Women</h3>
+          <ul>
+            <li>
+              <Link to="/search?q=dressess">Dresses</Link>
+            </li>
+            <li>
+              <Link to="/search?q=tops">Tops</Link>
+            </li>
+            <li>
+              <Link to="/search?q=Sweaters">Sweaters/Knits</Link>
+            </li>
+            <li>
+              <Link to="/search?q=Jackets">Jackets/Coats</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.link}>
+          <h3>Men</h3>
+          <ul>
+            <li>
+              <Link to="/search?q=Tees">Tees/Tank tops</Link>
+            </li>
+            <li>
+              <Link to="/search?q=Shirts">Shirts/Polos</Link>
+            </li>
+            <li>
+              <Link to="/search?q=Sweaters">Sweaters</Link>
+            </li>
+            <li>
+              <Link to="/search?q=Sweatshirts">Sweatshirts/Hoodies</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.link}>
+          <h3>About Us</h3>
+          <ul>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/location">Store Location</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders Tracking</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.link}>
+          <h3>Help</h3>
+          <ul>
+            <li>
+              <Link to="/shipping">Shipping & Returns</Link>
+            </li>
+            <li>
+              <Link to="/privacy">Privacy</Link>
+            </li>
+            <li>
+              <Link to="/t&C">Terms & Conditions</Link>
+            </li>
+            <li>
+              <Link to="/FAQ">FAQs</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <hr className={classes.hr} />
+      <hr />
       <div className={classes.bottom}>
-        <p>© 2021 Ecommerce. All rights reserved</p>
+        <p className={classes.text}>© 2021 Ecommerce. All rights reserved</p>
+        <div className={classes.cards}>
+          <i className="fab fa-cc-visa"></i>
+          <i className="fab fa-cc-mastercard"></i>
+          <i className="fab fa-cc-paypal"></i>
+        </div>
       </div>
     </div>
   )
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -2,133 +2,163 @@
 import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
     container: {
-        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "2rem",
         background: "#395B64",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        color: "white",
-        padding: "1rem 0",
+        color: "#fff",
         fontSize: "2rem",
-        justifyContent: "center",
-        transition: "all 0.3s ease-in-out",
+        "& hr": {
+            width: "100%",
+            border: "none",
+            height: "1px",
+            backgroundColor: "rgba(0,0,0,0.4)",
+            margin: "2rem 0",
+        },
     },
-    offer: {
+    top: {
+        display: "flex",
+        justifyContent: "space-between",
         width: "100%",
+        maxWidth: "1200px",
+        [theme.breakpoints.down("sm")]: {
+            flexDirection: "column",
+        },
+    },
+    Subscribe: {
         display: "flex",
         flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        "& h3": {
+            fontWeight: "800",
+            marginBottom: "1rem",
+        },
+        "& p": {
+            fontSize: "1.8rem",
+            fontWeight: "300",
+            marginBottom: "1rem",
+        },
+    },
+    input: {
+        display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        textAlign:"center",
-        '& h4': {
-            width:"30%",
-            fontSize: "3rem",
-            margin: "2rem 0",
-            [theme.breakpoints.down("md")]: {
-                width: "70%",
-            }
-        },
-        '& input': {
-            width: "25%",
-            fontSize: "1.5rem",
-            padding: "1.3rem",
+        "& input": {
+            width: "300px",
+            height: "4rem",
             border: "none",
-            borderRadius: "0.5rem",
             outline: "none",
-            [theme.breakpoints.down("md")]: {
-                width: "70%",
+            padding: "0 1rem",
+            fontSize: "1.8rem",
+            [theme.breakpoints.down("sm")]: {
+                width: "200px",
             },
         },
-        '& button': {
-            fontSize: "1.5rem",
-            margin: "1rem",
-            background: "#F2B705",
-            color: "black",
-            border: "none",
-            borderRadius: "0.5rem",
-            cursor: "pointer",
-            transition: "all 0.3s ease-in-out",
-            '&:hover': {
-                background: "black",
-                color: "#F2B705",
-            }
+        "& button": {
+            marginLeft: "1rem",
+            height: "4rem",
+            padding: "0 1rem",
+            fontSize: "1.8rem",
+            [theme.breakpoints.down("sm")]: {
+                marginLeft: "0.5rem",
+            },
         },
-       
     },
-    footer: {
-        width: "100%",
+    social: {
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         alignItems: "flex-start",
-        justifyContent: "space-evenly",
-        textAlign: "center",
-        [theme.breakpoints.down("md")]: {
-            flexDirection: "column",
+        justifyContent: "center",
+        "& h3": {
+            fontWeight: "800",
+            marginBottom: "1rem",
+        },
+        
+    },
+
+    icons: {
+            display: "flex",
+            alignItems: "center",
             justifyContent: "center",
-        },
-    },
-
-    footerLeft: {
-        width: "30%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        listStyle:"none",
-        '& h4': {
-            fontSize: "2rem",
-            margin: "2rem 0",
-        },
-        '& li': {
-            fontSize: "1.8rem",
-            margin: "1.3rem 0",
-        },
-        '& a': {
-            textDecoration: "none",
-            color: "white",
-            transition: "all 0.3s ease-in-out",
-            '&:hover': {
-                color: "#F2B705",
+            "& a": {
+                color: "#fff",
+                fontSize: "2rem",
+                margin: "0 1rem",
+                transition: "all 0.3s ease",
+                "&:hover": {
+                    color: "#000",
+                }
             }
         },
-        [theme.breakpoints.down("md")]: {
-            width: "100%",
-            alignItems: "center",
+ 
+    links: {
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%",
+        maxWidth: "1200px",
+        [theme.breakpoints.down("sm")]: {
+            flexDirection: "column",
         },
     },
-    footerRight: {
-        width: "30%",
+    link: {
         display: "flex",
         flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        "& h3": {
+            fontWeight: "800",
+            marginBottom: "1rem",
+        },
+        "& ul": {
+            listStyle: "none",
+            padding: "0",
+            margin: "0",
+            "& li": {
+                marginBottom: "0.5rem",
+                "& a": {
+                    color: "#fff",
+                    fontSize: "1.8rem",
+                    fontWeight: "300",
+                    textDecoration: "none",
+                    transition: "all 0.3s ease",
+                    "&:hover": {
+                        color: "#000",
+                    },
+                },
+            },
+        },
+    },
+    bottom: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        maxWidth: "1200px",
+        padding: "1rem 0",
+    },
+    text: {
+        fontSize: "1.8rem",
+        fontWeight: "300",
+        color: "#fff",
+        margin: "0 1rem",
+    },
+    cards:{
+        display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        listStyle:"none",
-        '& h4': {
-            fontSize: "2rem",
-            margin: "2rem 0",
-        },
-        '& li': {
-            fontSize: "1.8rem",
-            margin: "1.3rem 0",
-        },
-        '& a': {
-            textDecoration: "none",
-            color: "white",
-            transition: "all 0.3s ease-in-out",
-            '&:hover': {
-                color: "#F2B705",
+        "& i": {
+            fontSize: "3rem",
+            margin: "1rem",
+            color: "#fff",
+            transition: "all 0.3s ease",
+            "&:hover": {
+                color: "#000",
             }
-        },
-        [theme.breakpoints.down("md")]: {
-            width: "100%",
-            alignItems: "center",
-        },
+        }
+    }
 
-    },
-    hr:{
-        width:"99%",
-        height:"1px",
-        background:"white",
-        margin:"2rem 0"
-    },
 }))
